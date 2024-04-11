05dd8b914305e1aa89e7363d354317d269e66e9e
iamVL <166069151+iamVL@users.noreply.github.com>
2024-04-10 20:26:13 -0700

    Main.java
    Updated main.java with comments!

--- CSE360Phase2/src/application/Main.java
+++ CSE360Phase2/src/application/Main.java
@@ -1,17 +1,24 @@
 package application;
 
+// Import JavaFX classes needed.
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+// Main class to start the app.
 public class Main extends Application {
 
     @Override
+    // Start method sets up the main window.
     public void start(Stage primaryStage) {
+        // Create the main GUI and show it.
         MainGUI mainGUI = new MainGUI(primaryStage);
         mainGUI.showMainScreen();
     }
 
+    // Main method to run the app.
     public static void main(String[] args) {
+        // Start the JavaFX app.
         launch(args);
     }
 }
+
