7aec9a3785e4394ffa1b87955f30067549951b11
iamVL <166069151+iamVL@users.noreply.github.com>
2024-04-10 20:32:26 -0700

    MessageThread.java
    Updated MessageThread.java with comments

--- CSE360Phase2/src/application/MessageThread.java
+++ CSE360Phase2/src/application/MessageThread.java
@@ -1,21 +1,27 @@
 package application;
 
-// MessageThread.java
+// Importing necessary classes for handling collections.
 import java.util.ArrayList;
 import java.util.List;
 
+// Class to represent a thread of messages, like in a chat or email thread.
 public class MessageThread {
+    // A list to hold Message objects. This list represents the messages in the thread.
     private List<Message> messages;
 
+    // Constructor for MessageThread. Initializes the list of messages.
     public MessageThread() {
         this.messages = new ArrayList<>();
     }
 
+    // Method to add a new message to the thread.
     public void addMessage(Message message) {
         messages.add(message);
     }
 
+    // Getter method to retrieve all messages in the thread.
     public List<Message> getMessages() {
         return messages;
     }
 }
+
